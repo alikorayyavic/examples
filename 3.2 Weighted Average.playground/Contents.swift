0af75dc74a4440f52925ac3421a26bb9491c86cf
alikorayyavic <alikorayyavic@gmail.com>
2017-06-18 11:46:07 +0300

    3.2 Weighted Average
    You are given 3 grades stored in 3 variables of type Double finalsGrade, midtermGrade, projectGrade. These grades are used to compute the grade for a class. finalsGrade represents 50% of the grade. midtermGraderepresents 20% of the grade. projectGrade represents 30% of the final grade.
    Print the grade for the class.

---
+++ 3.2 Weighted Average.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+let 👽 = "hi, humans "
+
+
+var finalsGrade = 2.0
+var midtermGrade = 4.0
+var projectGrade = 3.0
+
+
+var gradeOfClass:Double = (finalsGrade*50/100 + midtermGrade*20/100 + projectGrade*30/100)
+
+print(gradeOfClass)
+
+/*Inputs	Your Output	Expected Output	Status
+ var finalsGrade = 2.0
+ var midtermGrade = 3.0
+ var projectGrade = 2.0
+ 2.2
+ 2.2
+ ✅
+ var finalsGrade = 5.0
+ var midtermGrade = 5.0
+ var projectGrade = 5.0
+ 5.0
+ 5.0
+ ✅
+ var finalsGrade = 1.0
+ var midtermGrade = 2.0
+ var projectGrade = 4.0
+ 2.1
+ 2.1
+ ✅
+ var finalsGrade = 2.0
+ var midtermGrade = 5.0
+ var projectGrade = 1.0
+ 2.3
+ 2.3
+ ✅
+ var finalsGrade = 4.0
+ var midtermGrade = 3.0
+ var projectGrade = 2.0
+ 3.2
+ 3.2
+ ✅*/
