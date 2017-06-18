11645d3b80eb94a6cedc9232110d62c07cd58f22
alikorayyavic <alikorayyavic@gmail.com>
2017-06-18 10:28:59 +0300

    3.1 Average
    +//You are given 2 Doubles a and b. Print their average.

---
+++ 👽.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//3.1 Average
+//You are given 2 Doubles a and b. Print their average.
+
+var a = 2.0
+var b = 5.0
+
+// your code here
+var avg:Double = (a + b) / 2
+
+
+print (avg)
