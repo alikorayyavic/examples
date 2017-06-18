88c8c91c969ae38a299282dabeada827c0f8c4b3
alikorayyavic <alikorayyavic@gmail.com>
2017-06-18 10:42:20 +0300

    Average 3.01
    You are given 2 Doubles a and b. Print their average.

--- 👽.playground/Contents.swift
+++ 👽.playground/Contents.swift
@@ -2,10 +2,7 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-//3.1 Average
-//You are given 2 Doubles a and b. Print their average.
+let 👽 = "hi, humans "
 
 var a = 2.0
 var b = 5.0
@@ -15,3 +12,33 @@ var avg:Double = (a + b) / 2
 
 
 print (avg)
+
+/*Inputs	Your Output	Expected Output	Status
+var a = 2.0
+var b = 5.0
+3.5
+3.5
+✅
+var a = 20.0
+var b = 10.0
+15.0
+15.0
+✅
+var a = 8.0
+var b = 3.0
+5.5
+5.5
+✅
+var a = 9.0
+var b = 12.0
+10.5
+10.5
+✅
+var a = 0.0
+var b = 9.0
+4.5
+4.5
+✅
+
+
+*/
